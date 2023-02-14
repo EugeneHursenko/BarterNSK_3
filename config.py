367951c0bdda555bebc13ba234b3b92739702b38
Art <you@example.com>
2023-02-14 17:18:53 +0700

    fixes

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-SQLALCHEMY_DATABASE_URI = 'sqlite:///database.db'
+SQLALCHEMY_DATABASE_URI = 'sqlite:///database.sqlite'
 SQLALCHEMY_ECHO = False
 SECRET_KEY = '!!! Нужно перегрузить в instance/config.py'
--- install.cmd
+++ install.cmd
@@ -1,6 +1,3 @@
 python -m pip install --user --no-warn-script-location virtualenv
 python -m venv venv
-.\venv\Scripts\activate && \
-    python -m pip install --upgrade pip && \
-    pip install --upgrade -r requirements.txt && \
-    flask db upgrade
+.\venv\Scripts\activate && python -m pip install --upgrade pip && pip install --upgrade -r requirements.txt && flask db upgrade
