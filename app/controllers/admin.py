c523e3aa4a0a88190f5bfbf88dd6f7331267c517
Art <you@example.com>
2023-05-01 15:59:47 +0700

    admin users list

--- app/controllers/admin.py
+++ app/controllers/admin.py
@@ -1,6 +1,6 @@
 from app import app, db
 from app.forms import CategoryForm
-from app.models import Category
+from app.models import Category, User
 from datetime import datetime
 from flask import abort, flash, redirect, render_template, request, url_for
 from flask_login import current_user, login_required
@@ -31,6 +31,17 @@ def admin_categories_index():
             flash('Категория создана', 'success')
             return redirect(url_for('admin_categories_index'))
 
-    categories = Category.query.all()
+    categories = Category.query.filter_by().order_by('title')
 
     return render_template('admin/categories_index.html', form=form, categories=categories)
+
+
+@app.route('/admin/users')
+@login_required
+def admin_users_index():
+    if current_user.role == 0:
+        return abort(403)
+
+    users = User.query.filter_by().order_by('id')
+
+    return render_template('admin/users_index.html', users=users)
